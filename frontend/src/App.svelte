1875b0ac3e2680f8cd93a359ce0a36b653214cba
hys <helpyourselfes@gmail.com>
2026-04-12 23:55:30 +0300

    Create graphArray type

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import logo from "./assets/images/logo-universal.png";
   import { onMount } from "svelte";
-  import { cpu, ram, startMonitoring } from "./statStore";
-    import Graph from "./lib/Graph.svelte";
+  import { cpuHistory, ramHistory, startMonitoring } from "./statStore";
+  const cpu = cpuHistory.array;
+  const ram = ramHistory.array;
+  import Graph from "./lib/Graph.svelte";
 
   onMount(() => startMonitoring(1000));
 </script>
@@ -11,12 +13,12 @@
   <div class="stats">
     <div class="stat-block cpu">
       <span class="stats-block__title">CPU {$cpu.at(-1)}%</span>
-      <Graph color="green" history={$cpu} width={500} height={100}/>
+      <Graph color="green" history={$cpu} width={500} height={100} />
     </div>
-    
+
     <div class="stat-block ram">
       <span class="stats-block__title">RAM {$ram.at(-1)}%</span>
-      <Graph color="red" history={$ram} width={500} height={100}/>
+      <Graph color="red" history={$ram} width={500} height={100} />
     </div>
   </div>
 </main>
--- frontend/src/statStore.ts
+++ frontend/src/statStore.ts
@@ -3,22 +3,24 @@ import { GetStats } from "../wailsjs/go/main/App";
 
 const MAX_LENGTH = 20;
 
-export const cpu = writable(new Array(MAX_LENGTH).fill(0));
-function updateCpu(value: number) {
-    cpu.update(arr => [...arr, value].slice(-MAX_LENGTH))
+function graphArray<T = number>(maxLength = 10) {
+    const array = writable(new Array<T>(maxLength));
+    const push = (value: T) => {
+        array.update(a => [...a, value].slice(-maxLength))
+    }
+
+    return { array, push }
 }
 
-export const ram = writable(new Array(MAX_LENGTH).fill(0));
-function updateRam(value: number) {
-    ram.update(arr => [...arr, value].slice(-MAX_LENGTH))
-}
+export const cpuHistory = graphArray(MAX_LENGTH)
+export const ramHistory = graphArray(MAX_LENGTH)
 
 
 export function startMonitoring(ms = 1000) {
     const interval = setInterval(async () => {
         const res = await GetStats();
-        updateCpu(Math.round(res.cpu))
-        updateRam(res.ram)
+        cpuHistory.push(Math.round(res.cpu))
+        ramHistory.push(res.ram)
     }, ms);
 
     return () => clearInterval(interval);
