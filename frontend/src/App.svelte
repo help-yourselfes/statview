3e4ad910cf1e53ce146bc3ce1dec53330ccd7922
hys <helpyourselfes@gmail.com>
2026-04-13 17:54:42 +0300

    Make graph dynamic

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -10,28 +10,31 @@
   import Graph from "./lib/Graph.svelte";
   import ProgressBar from "./lib/ProgressBar.svelte";
   import { derived } from "svelte/store";
+  import { activeGraph } from "./appStore";
 
   onMount(() => startMonitoring(1000));
+
+  const setActiveGraph = (value: graphType) => {
+    activeGraph.set(value);
+  };
 </script>
 
 <main>
+
   <div class="stats">
-    <div class="stat-block cpu">
+    <button class="stat-block cpu active" on:click={() => activeGraph.set("cpu")}>
       <span class="stats-block__title">CPU</span>
       <div class="progress-bar">
         <ProgressBar value={$cpuValue} />
       </div>
-      <div class="graph">
-        <Graph color="green" history={$cpu} />
-      </div>
-    </div>
+    </button>
 
-    <div class="stat-block ram">
+    <button class="stat-block ram" on:click={() => activeGraph.set("ram")}>
       <span class="stats-block__title">RAM {$ram.at(-1)}%</span>
-      <div class="graph">
-        <Graph color="red" history={$ram} />
-      </div>
-    </div>
+    </button>
+  </div>
+  <div>
+    <span>Active graph: {$activeGraph}</span>
   </div>
 </main>
 
@@ -41,9 +44,36 @@
     width: 10rem;
   }
   .stat-block {
+    background: none;
+    border: none;
     display: flex;
     flex-direction: row;
+    position: relative;
   }
+  .stat-block.active::before {
+    z-index: -1;
+    position: absolute;
+    display: block;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: color-mix(
+      in srgb,
+      var(--color) 100%,
+      var(--background) 100%
+    );
+  }
+
+  .cpu.active {
+    --color: var(--green);
+  }
+  .ram.active {
+    --color: var(--red);
+  }
+  .disk.active {
+    --color: var(--cyan);
+  }
+
   .graph {
     width: 100%;
     height: 40px;
